ad26913d7935759c5cd304748f9af9b3e3ea1ec1
Leonid Borisevich <dazzbourgh@gmail.com>
2023-06-20 22:01:07 -0500

    fixed job scope error with parent job finishing before child jobs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,20 @@
 import arrow.fx.coroutines.resourceScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import module.MainModule
 
 fun main() = runBlocking {
-    MainModule.modules.map {
-        val timeout = it.timeout
-        val checkerResource = it.checkerResource
-        val notifierResource = it.notifierResource
-
-        resourceScope {
-            val checker = checkerResource.bind()
-            val notifier = notifierResource.bind()
-            launch(Dispatchers.Default) { runProgram(Program(checker, notifier), timeout) }
-        }
-    }.forEach {
-        it.join()
+    resourceScope {
+        MainModule.modules.map {
+            val timeout = it.timeout
+            launch(Dispatchers.Default) {
+                runProgram(
+                    Program(it.checkerResource.bind(), it.notifierResource.bind()),
+                    timeout
+                )
+            }
+        }.joinAll()
     }
 }
